792791459b30536028372be6de695c6b97756934
Laupau8011 <laura.januleviciute@gmail.com>
2023-04-16 19:24:04 +0300

    finished d

--- 1_paskaita_savarankiskas/index.js
+++ 1_paskaita_savarankiskas/index.js
@@ -1,21 +1,21 @@
 const express = require("express");// express modulio importavimas
 const app = express();//aplikacijos sukurimas
-const port = 2900;// port (kanalo) skaicius
+const port = 3000;// port (kanalo) skaicius
 
 const casual = require("casual");
 
 
-//a +
-app.get("/randomUser", (req,res)=>{
-const randomUser = {
-    name: `${casual.first_name}`,
-    surname: `${casual.last_name}`,
-    country: `${casual.country}`,
-    city: `${casual.city}`,
-    street: `${casual.street}`,
-    zip: `${casual.zip}`
-}
-res.send(randomUser)
+//a +-
+app.get("/randomUser", (req, res) => {
+    const randomUser = {
+        name: `${casual.first_name}`,
+        surname: `${casual.last_name}`,
+        country: `${casual.country}`,
+        city: `${casual.city}`,
+        street: `${casual.street}`,
+        zip: `${casual.zip(digits = [5, 9])}`
+    }
+    res.send(randomUser)
 })
 
 
@@ -26,24 +26,29 @@ app.get("/randomColor", (req, res) => {
 })
 
 //c +
-app.get('/randomColors', (req, res) => {
+ app.get('/randomColors', (req, res) => {
     const randomColors = `Color name:['${casual.color_name}', '${casual.color_name}', '${casual.color_name}', '${casual.color_name}', '${casual.color_name}']`;
     res.send(randomColors);
-})
-app.get("/randomColors",(req,res)=>{
-    const colorArray=[];
-    for(let i=0;i<5; i++){
-        colorArray[i]=casual.safe_color_name;
+}) 
+
+app.get("/randomColors", (req, res) => {
+    const colorArray = [];
+    for (let i = 0; i < 5; i++) {
+        colorArray[i] = casual.color_name;
     }
     res.send(colorArray)
 })
-//d
-app.get('/randomPlaces', (req, res) => {
-    const randomPlaces = `Color name:['${casual.color_name}', '${casual.color_name}', '${casual.color_name}', '${casual.color_name}', '${casual.color_name}']`;
-})
-    res.send(randomColors);
 
  
+//d
+app.get('/randomPlaces', (req, res) => {
+    const placesArray = []
+    for (let i = 0; i < 5; i++) {
+        placesArray[i] = `[${casual.country}, ${casual.city}, ${casual.address}]`;
+    }
+    res.send(placesArray);
+});
+
 
 app.listen(port, () => {
     console.log(`Serveris listening on port ${port}`);
