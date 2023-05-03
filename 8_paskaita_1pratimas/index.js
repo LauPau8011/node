c97c26572f15b8080db0fa836c81bc0bb71813c5
Laupau8011 <laura.januleviciute@gmail.com>
2023-05-03 19:21:35 +0300

    class work

---
+++ 8_paskaita_1pratimas/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+// process.env -tai obkjektas sukurtas iš mūsų .env failo
+const port = process.env.PORT || 8080;
+//  8080-grįžtamasis ryšys jeigu PORT bus nerastas (atsarginis var.)
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// const ideas = [];
+
+app.get('/', (req, res) => {
+  res.send();
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}...`));
